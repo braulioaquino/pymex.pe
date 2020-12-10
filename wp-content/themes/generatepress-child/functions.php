9274e86e623a9a0214f0da0f8e98cdb263d0d9d6
Braulio Aquino [imac] <imac@braulioaquino.com>
2020-12-09 22:15:37 -0500

    Quitado de estilos de jetpack y gutemberg

--- wp-content/themes/generatepress-child/functions.php
+++ wp-content/themes/generatepress-child/functions.php
@@ -5,3 +5,15 @@
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
+
+// Quitando Gutemberg
+add_filter('use_block_editor_for_post', '__return_false', 10);
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+function smartwp_remove_wp_block_library_css(){
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' );
+}
+
+// Quitando estilos de jetpack del front
+add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
