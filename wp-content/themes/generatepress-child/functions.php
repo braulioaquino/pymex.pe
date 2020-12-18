f15aeedf4dcccdb99888a96a42f41220848f4788
Braulio Aquino [imac] <imac@braulioaquino.com>
2020-12-17 19:15:11 -0500

    ads archive

--- wp-content/themes/generatepress-child/functions.php
+++ wp-content/themes/generatepress-child/functions.php
@@ -17,3 +17,94 @@ function smartwp_remove_wp_block_library_css(){
 
 // Quitando estilos de jetpack del front
 add_filter( 'jetpack_implode_frontend_css', '__return_false', 99 );
+
+add_action( 'generate_before_main_content','f_generate_before_main_content' );
+function f_generate_before_main_content() {
+
+    if( is_archive() || is_home() ) {
+
+        if( !wp_is_mobile() ) { ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:336px;height:280px"
+                 data-ad-client="ca-pub-7512798297333598"
+                 data-ad-slot="6912692283"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            <?php
+        }
+        else { ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:336px;height:280px"
+                 data-ad-client="ca-pub-7512798297333598"
+                 data-ad-slot="2434676599"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            <?php
+        }
+
+        if( !wp_is_mobile() ) { ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-format="autorelaxed"
+                 data-ad-client="ca-pub-7512798297333598"
+                 data-ad-slot="1293009415"></ins>
+            <script>
+                 (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            <?php
+        }
+        else { ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-format="autorelaxed"
+                 data-ad-client="ca-pub-7512798297333598"
+                 data-ad-slot="5488269249"></ins>
+            <script>
+                 (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            <?php
+        }
+
+    }
+}
+
+add_action( 'generate_after_main_content','f_generate_after_main_content' );
+function f_generate_after_main_content() {
+
+    if( is_archive() || is_home() ) {
+
+        if( !wp_is_mobile() ) { ?>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    		<ins class="adsbygoogle"
+    			style="display:block"
+    			data-ad-client="ca-pub-7512798297333598"
+    			data-ad-slot="7653611709"
+    			data-ad-format="link"
+    			data-full-width-responsive="true"></ins>
+    		<script>
+    		(adsbygoogle = window.adsbygoogle || []).push({});
+    		</script>
+            <?php
+        }
+        else { ?>
+    		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    		<ins class="adsbygoogle"
+    			 style="display:block"
+    			 data-ad-client="ca-pub-7512798297333598"
+    			 data-ad-slot="3236799984"
+    			 data-ad-format="link"
+    			 data-full-width-responsive="true"></ins>
+    		<script>
+    		(adsbygoogle = window.adsbygoogle || []).push({});
+    		</script>
+            <?php
+        }
+
+    }
+}
